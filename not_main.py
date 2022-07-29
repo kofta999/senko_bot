afbf59e85c35e89ffa8190217909fa0b53bfdca0
Mostafa Mahmoud <99273340+kofta999@users.noreply.github.com>
2022-07-29 12:12:05 +0000

    new token

--- not_main.py
+++ not_main.py
@@ -8,7 +8,7 @@ waifuchoices = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'hug',
 subchoices = ['memes', 'cursedimages', 'dankmemes', 'cursedcomments', 'blessedimages', 'NoahGetTheBoat', 'aww',
               'FoodPorn', 'TIHI', 'MapPorn', 'HolUp', 'softwaregore', 'awwnime', 'wholesomememes']
 #bot token 
-token = 'ODU1NzY3NDUyMzMxMjc4MzY3.YM3RjA.FgWXRztFIl9pa625wR-Zbsp5AWI'
+token = 'ODU1NzY3NDUyMzMxMjc4MzY3.Gn0Kte.eX1q-ah9DzNGvsGC43MOgd0sg8I72y4elpr3qg'
 #reddit app stuff
 reddit_id = "7NEWIJoELE3LJEXtG4cjkw"
 reddit_secret = "A5RoK31eE9hKRBTEklvKaJVYLwSzCQ"
