4fe6117e7b7d2949af379947c378360e143aea3e
Mostafa Mahmoud <99273340+kofta999@users.noreply.github.com>
2022-10-11 01:02:07 +0200

    Update not_main.py

--- not_main.py
+++ not_main.py
@@ -8,9 +8,9 @@ waifuchoices = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'hug',
 subchoices = ['memes', 'cursedimages', 'dankmemes', 'cursedcomments', 'blessedimages', 'NoahGetTheBoat', 'aww',
               'FoodPorn', 'TIHI', 'MapPorn', 'HolUp', 'softwaregore', 'awwnime', 'wholesomememes']
 #bot token 
-token = 'ODU1NzY3NDUyMzMxMjc4MzY3.Gn0Kte.eX1q-ah9DzNGvsGC43MOgd0sg8I72y4elpr3qg'
+token = 'token_placeholder'
 #reddit app stuff
-reddit_id = "7NEWIJoELE3LJEXtG4cjkw"
-reddit_secret = "A5RoK31eE9hKRBTEklvKaJVYLwSzCQ"
-reddit_user = "Abghany"
-reddit_pass = "zamalekmetokba1911"
+reddit_id = "reddit_id_placeholder"
+reddit_secret = "reddit_secret_placeholder"
+reddit_user = "reddit_user_placeholder"
+reddit_pass = "reddit_pass_placeholder"
